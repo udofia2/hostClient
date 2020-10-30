50093eb4bb968ea17e1153ec009d4cdf09536602
udofia2 <enalsy22@gmail.com>
2020-10-30 18:44:55 +0100

    cloud storage connected

--- controller/domain.Controller.js
+++ controller/domain.Controller.js
@@ -1,9 +1,15 @@
 const whois = require("whois");
 const fs = require("fs");
-const domainActions = (Domains) => {
+const mongoose = require("mongoose");
+
+const Domains = require("./../model/domain.model");
+
+const domainActions = () => {
+  //A function that makes a request to whois for available domains
   const newDomain = async (req, res) => {
     const { name } = req.body;
 
+    //Takesn out the extension in a given domain
     const exp = /(\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/;
 
     const domainWithoutSuffix = name.slice(0, name.match(exp).index);
@@ -26,20 +32,20 @@ const domainActions = (Domains) => {
       ".space",
     ];
 
-    const whoisP = (domain, opts = {}) => {
+    const whoisP = async (domain, opts = {}) => {
+      //Makes a new lookup to whois
       return new Promise((resolve, reject) => {
-        whois.lookup(domain, opts, (err, data) => {
+        whois.lookup(domain, opts, async (err, data) => {
           if (err) reject(err);
 
-          // fs.appendFile("whos.json", data, (err) => {
-          //   if (err) throw err;
-          // });
+          //informs users that the domain is available
           if (data.trim().startsWith("No")) {
             resolve(data);
             console.log(`${domain} is available`);
-            return res.json(`${domain} is available`);
+            return res.json(domain);
           }
 
+          //For domains that are not available, sugestions are made
           suggestions();
         });
       });
@@ -50,35 +56,48 @@ const domainActions = (Domains) => {
         const domainModified = `${domainWithoutSuffix}${sufix}`;
 
         whois.lookup(domainModified, async (err, data) => {
-          console.log("i got hit");
           if (err) throw err;
 
           if (data.trim().startsWith("Domain")) {
             console.log(`${domainModified} is taken`);
           } else {
+            //saves availabe domains to the database
             const more = new Domains({
               name: domainModified,
             });
-
             await more.save();
             console.log(domainModified);
           }
         });
       });
 
-      const found = await Domains.find({});
+      //waits for seconds before redirecting to fetch all available doamins from dtabase
+      setTimeout(() => {
+        res.redirect("/api/domain/hit");
+        
+      }, 5000);
 
-      res.json({
-        msg: `available domains`,
-        found,
+      //clear the database for a new request
+
+      await mongoose.connection.db.dropCollection("domains", (err, result) => {
+        console.log("Collection dropped");
+        //     res.json({
+        //       msg: "just something to test",
+        //       found,
+        //     });
       });
-
-      await Domains.deleteMany();
     };
 
     await whoisP(name);
   };
 
+  const display = async (req, res) => {
+    const found = await Domains.find({});
+    res.render("list", {
+      msg: `available domains`,
+      found,
+    });
+  };
   const domains = async (req, res) => {
     res.render("domain");
   };
@@ -86,6 +105,7 @@ const domainActions = (Domains) => {
   return {
     domains,
     newDomain,
+    display,
   };
 };
 
--- routes/domain.route.js
+++ routes/domain.route.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const domainRouter = express.Router()
 const Domains = require('./../model/domain.model')
-const { newDomain, domains } = require('./../controller/domain.Controller')(Domains)
+const { newDomain, domains, display } = require('./../controller/domain.Controller')(Domains)
 // const { a } = require('./../controller/try.Controller')()
 
+domainRouter.route('/hit').get(display)
 domainRouter.route('/').post(newDomain)
 domainRouter.route('/').get(domains)
-// domainRouter.route('/try').get(a)
 
 
 module.exports = domainRouter
