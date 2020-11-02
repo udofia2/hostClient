95bc2f8ccea31f82381d7d33cce80ed30567c838
udofia2 <enalsy22@gmail.com>
2020-11-02 17:07:07 +0100

    removed unnecessary files

--- routes/domain.route.js
+++ routes/domain.route.js
@@ -3,9 +3,10 @@ const domainRouter = express.Router()
 const Domains = require('./../model/domain.model')
 const {  newDomain,index, result, } = require('./../controller/domain.Controller')(Domains)
 
-
 domainRouter.route('/').post(newDomain)
+//Home page
 domainRouter.route('/').get(index)
+//Result page
 domainRouter.route('/result').get(result)
 
 module.exports = domainRouter
